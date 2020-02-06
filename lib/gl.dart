7395f0bbe2361c029f548f058f11c3bc811bad89
Alexander Vinogradov <armacoty@protonmail.com>
2020-02-06 17:43:01 +0300

    functions added to gl.dart

--- lib/gl.dart
+++ lib/gl.dart
@@ -1,4 +1,9 @@
 library gl;
 
+import 'src/core/functions.dart';
+
 export 'src/core/gl_constants.dart';
 export 'src/core/gl_pfn_typedefs.dart';
+export 'src/core/functions.dart';
+
+Gl gl = new Gl();
